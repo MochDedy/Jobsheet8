39c8d3be3ae434f7405c59d13a8675a25f3e890b
MochDedy <mochdedy96@gmail.com>
2025-10-28 00:48:46 +0700

    Bintang Segitiga

--- Triangle14.java
+++ Triangle14.java
@@ -1,11 +1,10 @@
-import java.net.SocketImpl;
 import java.util.Scanner;
 
 public class Triangle14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n, j, i = 0;
+        int n, j, i = 1;
 
         System.out.print("Masukkan nilai n: ");
         n = sc.nextInt();
