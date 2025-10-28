81a3fcbb4ea11d4f5ee6f2dd4944518873bd228e
MochDedy <mochdedy96@gmail.com>
2025-10-28 11:10:47 +0700

    Percobaan 4: Studi Kasus Nilai Tugas Proyek Kelompok

---
+++ NilaiKelompok14.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NilaiKelompok14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, nilai;
+        float totalNilai, rataNilai;
+
+        System.out.println("==== Program Penilaian Tugas Kelompok ====");
+        System.out.println("Terdapat 6 kelompok dalam 1 kelas");
+        System.out.println("Setiap kelompok akan dinilai oleh 5 kelompok lainnya!");
+        System.out.println("Silahkan masukkan nilai dengan rentan 1 - 100.\n");
+
+        i = 1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+
+            totalNilai = 0;
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("Rata-rata nilai Kelompok" + i + "= " + rataNilai);
+            i++;
+        }
+    }
+}
